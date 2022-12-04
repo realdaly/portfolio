b2abb7779992d1dc26e54619ea54b218bed40de0
realdaly <mohammed96218@yahoo.com>
2022-12-04 15:53:28 +0300

    changed fields to apply to the design

--- api/models.py
+++ api/models.py
@@ -2,49 +2,51 @@ from django.db import models
 
 # Create your models here.
 class About(models.Model):
-    greet = models.CharField(max_length=1000)
-    info = models.CharField(max_length=5000)
-    paragraph = models.TextField(max_length=1000)
-    image = models.CharField(max_length=5000)
+    name = models.CharField(max_length=1000, default="Empty")
+    title = models.CharField(max_length=1000, default="Empty")
+    paragraph = models.TextField(max_length=5000, default="Empty")
+    profileImg = models.CharField(max_length=5000, default="Empty")
+    coverImg = models.CharField(max_length=5000, default="Empty")
 
     def __str__(self):
-        return f"{self.greet}"
+        return f"{self.name}, {self.title}"
 
 
 class Education(models.Model):
     year = models.IntegerField()
-    description = models.TextField(max_length=5000)
+    description = models.TextField(max_length=5000, default="Empty")
 
     def __str__(self):
         return f"{self.year}"
 
 
 class Skill(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=1000, default="Empty")
 
     def __str__(self):
         return f"{self.name}"
 
 
 class Project(models.Model):
-    image = models.CharField(max_length=5000)
-    link = models.CharField(max_length=1000)
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=1000, default="Empty")
+    image = models.CharField(max_length=5000, default="Empty")
+    link = models.CharField(max_length=5000, default="Empty")
 
     def __str__(self):
         return f"{self.name}"
 
 
 class Hobby(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=1000, default="Empty")
 
     def __str__(self):
         return f"{self.name}"
 
 
 class Contact(models.Model):
-    name = models.CharField(max_length=100)
-    link = models.CharField(max_length=1000)
+    name = models.CharField(max_length=1000, default="Empty")
+    content = models.CharField(max_length=5000, default="Empty")
+    link = models.CharField(max_length=5000, default="Empty")
 
     def __str__(self):
         return f"{self.name}: {self.link}"
